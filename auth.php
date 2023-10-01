7ac2a3c179a9c487453bb1e621355cf2af9ac869
Brighton Brown <brizziehh@gmail.com>
2023-10-01 15:39:23 +0300

    remove session controll

--- auth.php
+++ auth.php
@@ -3,9 +3,5 @@
     if(!isset($_SESSION['uid'])) {
         header("Location: login.php");
     }
-    
-    // if(isset($_SESSION['uid'])){
-    //     header("Location: dashboard/index.php");
-    // }
 
 ?>
